4358ca833b293db247d8ebea4fa98c271905e86a
Ammar1999y <thesyclashers.911@gmail.com>
2024-01-21 02:23:02 +0300

    first commit

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,8 +1,8 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'get from themoviedb.org',
-    originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
-}
+  baseUrl: "https://api.themoviedb.org/3/",
+  apiKey: "bcee296b5552b8a4ad64436153b59fb9",
+  originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
+  w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
+};
 
 export default apiConfig;
